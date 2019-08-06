9a01ca6157d759dc4412507521d4e308376e5056
Wonpyoung <cwp10@msn.com>
2019-08-06 18:02:57 +0900

    UI 에 ray포인터가 감지 할 수 있도록 자동으로 박스컬리더를 생성하는 스크립트 제작

---
+++ Assets/Scripts/UICollider.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class UICollider : MonoBehaviour 
+{
+	private BoxCollider _boxCollider = null;
+	private RectTransform _rectTransform = null;
+
+	private void OnEnable()
+	{
+		ValidateCollider();
+	}
+
+	private void OnValidate()
+	{
+		ValidateCollider();
+	}
+
+	private void ValidateCollider()
+	{
+		_rectTransform = GetComponent<RectTransform>();
+
+		_boxCollider = GetComponent<BoxCollider>();
+
+		if (_boxCollider == null)
+		{
+			_boxCollider = this.gameObject.AddComponent<BoxCollider>();
+		}
+
+		_boxCollider.size = _rectTransform.sizeDelta;
+	}
+}
